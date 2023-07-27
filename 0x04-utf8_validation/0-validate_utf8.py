952c832f407332f949b6e94a6bfee2f6537b12e9
Dennis Murage <dmurage140@gmail.com>
2023-07-27 18:44:30 +0300

    Added task 0 files and README.md

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+def validUTF8(data):
+    remaining_bytes = 0
+
+    for num in data:
+        # Check if the current integer starts a valid UTF-8 character sequence
+        if remaining_bytes == 0:
+            if (num >> 7) == 0b0:
+                remaining_bytes = 0
+            elif (num >> 5) == 0b110:
+                remaining_bytes = 1
+            elif (num >> 4) == 0b1110:
+                remaining_bytes = 2
+            elif (num >> 3) == 0b11110:
+                remaining_bytes = 3
+            else:
+                return False
+        else:
+            # Check if the current integer starts with the pattern '10'
+            if (num >> 6) != 0b10:
+                return False
+            remaining_bytes -= 1
+
+    # If all bytes are correctly processed, there should be no remaining bytes
+    return remaining_bytes == 0
