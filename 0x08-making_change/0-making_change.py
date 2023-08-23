e41ca37446db03bde0c91686b816d3abe95e915e
Dennis Murage <dmurage140@gmail.com>
2023-08-23 21:18:06 +0300

    Implemented finding change function

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""finds minimum coins for change"""
+
+
+def makeChange(coins, total):
+    """_summary_
+
+    Args:
+        coins (_type_): _description_
+        total (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    coins.sort(reverse=True)
+    i = 0
+    counter = 0
+
+    for i in range(len(coins)):
+        while (total >= coins[i]):
+            total -= coins[i]
+            counter += 1
+
+    if total != 0:
+        return -1
+
+    return counter
